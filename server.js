0fd9b8b8da7c38d7b688539467ef2a2e0ce5adb4
Your Name <you@example.com>
2022-06-23 15:02:44 -0700

    Created get all and get one routes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const db = require("./db/conn");
+const db = require("./database/conn");
 const app = express();
 const cors = require('cors');
 
@@ -10,6 +10,86 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(cors());
 
+//getall
+app.get("/specs/general", async (req, res) => {
+    try {
+        let client = await db.connect();
+        const result = await db.query("SELECT * FROM general");
+        res.json(result.rows);
+        console.log(result.rows);
+        client.release();
+    } catch (error) {
+        console.error(error);
+        res.send("Error: ", error);
+    }
+})
+
+//getall
+app.get("/specs/warranty", async (req, res) => {
+    try {
+        let client = await db.connect();
+        const result = await db.query("SELECT * FROM warranty");
+        res.json(result.rows);
+        console.log(result.rows);
+        client.release();
+    } catch (error) {
+        console.error(error);
+        res.send("Error: ", error);
+    }
+})
+
+//getall
+app.get("/specs/other", async (req, res) => {
+    try {
+        let client = await db.connect();
+        const result = await db.query("SELECT * FROM other");
+        res.json(result.rows);
+        console.log(result.rows);
+        client.release();
+    } catch (error) {
+        console.error(error);
+        res.send("Error: ", error);
+    }
+})
+
+//getone 
+app.get("/specs/general/:id", async (req, res) => {
+    try {
+        let client = await db.connect();
+        const result = await db.query("SELECT * FROM general WHERE id = $1", [req.params.id]);
+        res.json(result.rows);
+        client.release();
+    } catch (error) {
+        console.error(error);
+        res.send("Error: ", error);
+    }
+})
+
+//getone 
+app.get("/specs/warranty/:id", async (req, res) => {
+    try {
+        let client = await db.connect();
+        const result = await db.query("SELECT * FROM warranty WHERE id = $1", [req.params.id]);
+        res.json(result.rows);
+        client.release();
+    } catch (error) {
+        console.error(error);
+        res.send("Error: ", error);
+    }
+})
+
+//getone 
+app.get("/specs/other/:id", async (req, res) => {
+    try {
+        let client = await db.connect();
+        const result = await db.query("SELECT * FROM other WHERE id = $1", [req.params.id]);
+        res.json(result.rows);
+        client.release();
+    } catch (error) {
+        console.error(error);
+        res.send("Error: ", error);
+    }
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening on Port ${process.env.PORT}`);
